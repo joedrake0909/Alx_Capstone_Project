83071c26672cafc27e8c0d37e0e14b92e0c5b337
Joseph_Bio <josephbiossey03@gmail.com>
2025-12-09 12:11:44 +0000

    feat(admin): Register Group, Cycle, Member, and Contribution models for management

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Group, Member, Cycle
+
 
 # Register your models here.
+admin.site.register(Group)
+admin.site.register(Member)
+admin.site.register(Cycle)
+
+
