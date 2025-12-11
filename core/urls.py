2bb8d1475b6a23937f2e60c77cd4e0ff6ee55fe8
Joseph_Bio <josephbiossey03@gmail.com>
2025-12-11 04:18:34 +0000

    Add authentication URL routing with login/logout redirects

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include('django.contrib.auth.urls')),
+    path("", RefirectView.as_view(pattern_name='login'), name='root' ),
+    path('app/', include('groups.urls')),
+    path('app/', include('contributions.urls')),
+
 ]
