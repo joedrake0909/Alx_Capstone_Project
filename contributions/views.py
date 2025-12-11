c88043bca3936ae55bf65f78535cbee1d3ec0367
Joseph_Bio <josephbiossey03@gmail.com>
2025-12-11 22:06:45 +0000

    establish secure, role-gated foundation for admin dashboard

--- contributions/views.py
+++ contributions/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
+from django.shortcuts import redirect
+
+# Check if user is admin
+def is_admin(user):
+    return user.is_superuser
+
+# The core admin dashboard view
+@login_required(login_url='login')
+@user_passes_test(is_admin, login_url='login')
+
+def dashboard_view(request):
+    #Admin landing page showing group totals
+
+    context = {
+        'page_title': 'Admin Dashboard',
+        'admin_message': 'Welcome to the Admin Control Panel.This view is secured!',
+    }
+    return render(request, 'dashboard.html', context)
+
+
+
+
 
 # Create your views here.
